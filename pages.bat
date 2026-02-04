2b7f5be4d70faea949306c5add3d9563ca313ce4
Adrian Johnstone <a.johnstone@rhul.ac.uk>
2026-02-04 17:01:34 +0000

    More text; re-enabled multilingual capability

--- pages.bat
+++ pages.bat
@@ -1,5 +1,5 @@
 @echo off
 call amp en.amp events.amp groups.amp marketplace.amp connections.amp suppliers.amp g1mra.amp 
-rem call amp fr.amp events.amp g1mra.amp
-rem call amp de.amp events.amp g1mra.amp
-copy home_en.html index.html > nul
+call amp de.amp events.amp groups.amp marketplace.amp connections.amp suppliers.amp g1mra.amp 
+call amp fr.amp events.amp groups.amp marketplace.amp connections.amp suppliers.amp g1mra.amp 
+copy Home_en.html index.html > nul
