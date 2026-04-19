fe7d52a8bc122d462d8bf56569508120f364515d
Adrian Johnstone <a.johnstone@rhul.ac.uk>
2026-04-19 16:32:54 +0100

    After shift to machine generated translation macro names

--- pages.bat
+++ pages.bat
@@ -1,5 +1,4 @@
-@echo off
-call amp en.amp events.amp groups.amp marketplace.amp connections.amp suppliers.amp g1mra.amp 
-call amp de.amp events.amp groups.amp marketplace.amp connections.amp suppliers.amp g1mra.amp 
-call amp fr.amp events.amp groups.amp marketplace.amp connections.amp suppliers.amp g1mra.amp 
+call amp en.amp          events.amp groups.amp marketplace.amp connections.amp suppliers.amp g1mra.amp 
+call amp de.amp de_T.amp events.amp groups.amp marketplace.amp connections.amp suppliers.amp g1mra.amp 
+call amp fr.amp fr_T.amp events.amp groups.amp marketplace.amp connections.amp suppliers.amp g1mra.amp 
 copy Home_en.html index.html > nul
