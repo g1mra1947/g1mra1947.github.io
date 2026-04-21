a3727ce1978e8785da65956ede8c7e32554c1e63
Adrian Johnstone <a.johnstone@rhul.ac.uk>
2026-04-21 13:22:54 +0100

    Some limited translations

--- pages.bat
+++ pages.bat
@@ -1,4 +1,4 @@
 call amp en.amp          events.amp groups.amp marketplace.amp connections.amp suppliers.amp g1mra.amp 
-call amp de.amp de_T.amp events.amp groups.amp marketplace.amp connections.amp suppliers.amp g1mra.amp 
-call amp fr.amp fr_T.amp events.amp groups.amp marketplace.amp connections.amp suppliers.amp g1mra.amp 
+call amp de.amp amp_T_de.amp events.amp groups.amp marketplace.amp connections.amp suppliers.amp g1mra.amp 
+call amp fr.amp amp_T_fr.amp events.amp groups.amp marketplace.amp connections.amp suppliers.amp g1mra.amp 
 copy Home_en.html index.html > nul
